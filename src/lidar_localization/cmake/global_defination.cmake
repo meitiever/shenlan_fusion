62d59987f0da51c44e0e50204bd374cbc6283013
meitiever <meitiever@gmail.com>
2021-02-01 15:14:42 +0800

    ss

--- src/lidar_localization/cmake/global_defination.cmake
+++
@@ -1,5 +0,0 @@
-set(WORK_SPACE_PATH /home/sineva-sys-master/fusion_ws/src/01-lidar-odometry/src/lidar_localization)
-configure_file (
-  ${PROJECT_SOURCE_DIR}/include/${PROJECT_NAME}/global_defination/global_defination.h.in
-  ${PROJECT_BINARY_DIR}/include/${PROJECT_NAME}/global_defination/global_defination.h)
-include_directories(${PROJECT_BINARY_DIR}/include)
